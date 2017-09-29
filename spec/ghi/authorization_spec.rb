793a0e71b7ddd8b89b2b46f93da6f0433aaa92dc
Joseph W Becher <jwbecher@drazisil.com>
2017-09-29 19:45:39 -0400

    Fixed test

--- spec/ghi/authorization_spec.rb
+++ spec/ghi/authorization_spec.rb
@@ -3,10 +3,10 @@ require "ghi"
 describe GHI::Authorization do
   describe "token" do
     context "given an empty string" do
-	    subject {GHI::Authorization::Required.new}
-	    it "moo" do
-		    expect(subject.message()).to eql("foo")
-	    end
+      subject {GHI::Authorization::Required.new}
+      it "moo" do
+        expect(subject.message()).to eql("Authorization required.")
+      end
       it "returns zero" do
         expect(GHI::Authorization.token()).to eql(nil)
       end
