60a157a56454cdc8910ddcb97c2c2c750230c196
Joseph W Becher <jwbecher@drazisil.com>
2017-09-29 18:34:53 -0400

    Added Gemfile.lock and another test

--- spec/ghi/authorization_spec.rb
+++ spec/ghi/authorization_spec.rb
@@ -3,7 +3,10 @@ require "ghi"
 describe GHI::Authorization do
   describe "token" do
     context "given an empty string" do
-
+	    subject {GHI::Authorization::Required.new}
+	    it "moo" do
+		    expect(subject.message()).to eql("foo")
+	    end
       it "returns zero" do
         expect(GHI::Authorization.token()).to eql(nil)
       end
